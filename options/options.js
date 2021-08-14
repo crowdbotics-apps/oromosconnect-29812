8d9acdbec2121a53929bac11d1fba4b4fb57247b
Crowdbotics <team@crowdbotics.com>
2021-08-14 19:55:05 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "oromosconnect_29812Identifier",
+  url: "https://oromosconnect_29812Identifier.botics.co",
+  api: "https://oromosconnect_29812Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
